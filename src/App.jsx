a6d83ee43e54d2a45a22cb1e60e4b7f3a11b049a
ggpio18 <58065866+ggpio18@users.noreply.github.com>
2024-05-21 22:27:02 +0800

    2nd commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Home from "./components/pages/home/Home";
+
+
+function App() {
+  return (
+    <>
+    <Router>
+      <Routes>
+        <Route path="/home" element={<Home/>}/>
+      </Routes>
+    </Router>
+    </>
+  );
+}
+export default App;
